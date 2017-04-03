b46f811d84f0f60960aba9314202981d489e4713
kshakirov <kshakirov@zoral.com.ua>
2017-04-03 14:08:51 +0300

    DONE  #47 applications a few changes

--- tools/search/application/application_indexer.rb
+++ tools/search/application/application_indexer.rb
@@ -3,8 +3,10 @@ elastic_host = get_elastic_host
 tcas_host = get_tcas_host
 
 index_manager = TurboCassandra::ElasticIndex.new(elastic_host, tcas_host)
-
+counter = 1
 YAML.load_stream(File.open(File.expand_path( '../data/all_applications.yml', File.dirname(__FILE__)))) do |document|
+  puts "Added Document [#{counter}]"
   index_manager.add_application document
+  counter += 1
 end
 
