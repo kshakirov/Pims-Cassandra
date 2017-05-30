cb630f7029560c8215f082b562054f439310ea85
kshakirov <kshakirov@zoral.com.ua>
2017-05-30 13:18:52 +0300

    test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,30 +1,24 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'json'
 require 'yaml'
-require 'logger'
 require 'cassandra'
+require "march_hare"
 require 'active_support'
 require 'active_support/all'
-require_relative '../lib/tools/Model/turbo_cluster'
-require_relative '../lib/tools/Model/product/product'
-require_relative '../lib/tools/Model/customer/customer'
-require_relative '../lib/tools/Model/order/order'
-require_relative '../lib/tools/Model/attribute'
-require_relative '../lib/tools/Model/order/order_batch'
-require_relative '../lib/tools/Model/visitor_log/visitor_logs'
-require_relative '../lib/tools/Model/visitor_log/customer_logs'
-require_relative '../lib/tools/API/visitor_log/visitor_logs'
-require_relative '../lib/tools/API/product/product'
-class MyCluster
-  def initialize
-    cluster =Cassandra.cluster(hosts: ['10.1.3.15', '10.1.3.16', '10.1.3.17'])
-    keyspace = 'trash1'
-    @session = cluster.connect(keyspace)
-  end
+require 'active_support/core_ext/hash/compact'
+require 'active_directory'
+require 'jwt'
+require 'erb'
+require 'action_view'
+require 'action_view/helpers'
+require 'prawn'
+require 'prawn/table'
+require 'carmen'
+require 'carmen/country'
+require_relative '../lib/sources'
+ENV['TURBO_MODE'] = "development"
 
-  def execute cql, args
-    insert = @session.prepare(cql)
-    @session.execute(insert, arguments: args, consistency: :any)
-  end
-end
 
 def read_product_from_file
   YAML.load_file('../all_products.yml')
